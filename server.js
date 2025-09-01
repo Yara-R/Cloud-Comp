e383f183db11dd63094624d21d868b2ce9a15057
Yara Rodrigues Inácio <yara.rodrigues.inacio@gmail.com>
2025-09-01 11:55:59 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 10000;
+
+// Serve arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log(`Server rodando na porta ${port}`);
+});
